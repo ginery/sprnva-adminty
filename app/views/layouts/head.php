af5b2d8248bca56b0c5795191e72a879eb1b4378
Ginery <songalingginery@gmail.com>
2021-06-09 16:40:01 +0800

    additional updates in datatables

--- app/views/layouts/head.php
+++ app/views/layouts/head.php
@@ -39,11 +39,11 @@ use App\Core\Auth;
 	<script src="<?= public_url('/assets/adminty/bower_components/modernizr/js/modernizr.js') ?>"></script>
 
 	<!-- datatables -->
-	<script src="<?= public_url('/assets/adminty/bower_components/datatables.net/js/jquery.dataTables.min.js') ?>"></script>
+	<script src="<?= public_url('/assets/adminty/bower_components/datatables.net/js/jquery.dataTables.js') ?>"></script>
 	<script src="<?= public_url('/assets/adminty/bower_components/datatables.net-buttons/js/dataTables.buttons.min.js') ?>"></script>
 	<script src="<?= public_url('/assets/adminty/bower_components/datatables.net-buttons/js/buttons.print.min.js') ?>"></script>
 	<script src="<?= public_url('/assets/adminty/bower_components/datatables.net-buttons/js/buttons.html5.min.js') ?>"></script>
-	<script src="<?= public_url('/assets/adminty/bower_components/datatables.net-bs4/js/dataTables.bootstrap4.min.js') ?>"></script>
+	<script src="<?= public_url('/assets/adminty/bower_components/datatables.net-bs4/js/dataTables.bootstrap4.js') ?>"></script>
 	<script src="<?= public_url('/assets/adminty/bower_components/datatables.net-responsive/js/dataTables.responsive.min.js') ?>"></script>
 	<script src="<?= public_url('/assets/adminty/bower_components/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') ?>"></script>
 
